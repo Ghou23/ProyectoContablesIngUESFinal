62a66607080492bf53e036315496c9dfa7e49c79
josedelao <Ing.josedelaoK021@gmail.com>
2015-11-04 23:25:19 -0600

    esta es la segunda prueba

---
+++ contabilidad mod/src/almacen2/data/prueba02.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package almacen2.data;
+
+/**
+ *
+ * @author Zeifodias
+ */
+public class prueba02 {
+ 
+    String primero; 
+    int segundo;
+    double pase;
+    
+}
